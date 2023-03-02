efb70b2b09c7d3138cebbe5a5fdc6f0ac58e4131
cool kid <58775186+CK4869@users.noreply.github.com>
2023-03-02 10:19:38 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,73 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
 
-String text = '';
+String apiKey = '';
 void main() {
-  text = const String.fromEnvironment('API_KEY', defaultValue: 'Hello World!');
+  apiKey =
+      const String.fromEnvironment('API_KEY', defaultValue: 'Hello World!');
   runApp(const MainApp());
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends StatefulWidget {
   const MainApp({super.key});
+
+  @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+  String? img;
+
+  Future<void> createImage({
+    String textDescription = 'cat',
+    int quantity = 1,
+    String size = '256x256',
+  }) async {
+    final response = await http.Client().post(
+      Uri.parse('https://api.openai.com/v1/images/generations'),
+      headers: {
+        'Authorization': 'Bearer $apiKey',
+        'Content-Type': 'application/json',
+      },
+      body: jsonEncode({
+        'prompt': textDescription,
+        'n': quantity,
+        'size': size,
+      }),
+    );
+
+    if (response.statusCode == 200) {
+      final imageUrl = jsonDecode(response.body)['data'][0]['b64_json'];
+      setState(() {
+        img = imageUrl;
+      });
+    } else {
+      img = null;
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text(text + 'huh'),
+        body: Column(
+          children: [
+            const Text('haha'),
+            FilledButton(
+              onPressed: () async {
+                createImage();
+              },
+              child: const Text('button'),
+            ),
+            img != null
+                ? SizedBox(
+                    width: 300,
+                    height: 300,
+                    child: Image.network(img!),
+                  )
+                : const Text('NULL'),
+          ],
         ),
       ),
     );
